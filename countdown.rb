711a9f2900e3f390c1b95af49af4df3bf90ecea9
Chris Baum <christopher.baum@valpo.edu>
2021-01-11 02:42:16 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -1,15 +1,14 @@
 #write your code here
 
-#number = 10
 def countdown(number)
   while number > 1
-    puts "#{number}"
+    #puts "#{number}"
     number -= 1
-    puts "#{number} SECOND(S)!"
+    #puts "#{number} SECOND(S)!"
   end
   "HAPPY NEW YEAR!"
 end
-#countdown(number)
+countdown(10)
 
 def countdown_with_sleep
 
